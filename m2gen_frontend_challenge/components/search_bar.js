595cb607f84802d65237a5869be10a68b7f2f96e
Andrew Bigby <Andrew Bigby>
2016-12-19 05:56:58 -0500

    completed Basic ReactJS integration, updated responsive css, etc.

--- m2gen_frontend_challenge/components/search_bar.js
+++ m2gen_frontend_challenge/components/search_bar.js
@@ -4,19 +4,21 @@ export class SearchBar extends React.Component {
   render() {
     return(
       <nav className="navbar navbar-inverse">
-        <div class="container-fluid">
-          <form className="navbar-form navbar-left">
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="Search"/>
-              <div className="input-group-btn">
-                <button className="btn btn-default" type="submit">
-                  <i className="glyphicon glyphicon-search"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-          <h1 id="searchbar-phrase">Find Your Favorite of 2016</h1>
+        <div className="container">
+          <div className="col-xs-12 col-md-6">
+            <h1 id="searchbar-phrase" className="text-center">Find Your Favorite of 2016</h1>
           </div>
+          <div className="col-xs-12 col-md-6">
+              <div className="input-group m-10">
+                <input id="searchFilter" type="text" className="form-control" placeholder="Search"/>
+                <div className="input-group-btn">
+                  <button className="btn btn-default" type="submit">
+                    <i className="glyphicon glyphicon-search"></i>
+                  </button>
+                </div>
+               </div>
+          </div>
+        </div>
       </nav>
     );
   }
